8eb1e51b5b1e6fae53f5de6849323935165c4da3
Richard Astbury <richard.astbury@gmail.com>
2019-04-17 15:53:04 +1000

    tidy up

--- engine/storyboard.js
+++ engine/storyboard.js
@@ -1,8 +1,8 @@
 module.exports = level => {
   let gameTime = 0
-  let actions = []
+  const actions = []
 
-  // we're passsed in the time to add since the last action,
+  // we're passed in the time to add since the last action,
   // rather than the absolute time
   level((time, action) => {
     gameTime += time
@@ -18,9 +18,6 @@ module.exports = level => {
       const actionsToExecute = actions.filter(x => {
         return x.gameTime > lastGameTime && x.gameTime <= ctx.gameTime
       })
-      if (actionsToExecute.length){
-        console.log('found actions')
-      }
       actionsToExecute.forEach(x => {
         x.action(ctx)
       })
